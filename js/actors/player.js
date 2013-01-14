3f83ef3f3aa45c271530c9bafe9c0db149bff776
Jake Kaufman <theevocater@gmail.com>
2013-01-14 01:55:13 -0800

    Add basic player stuff. doesn't really work
    I think we should change to click to move. not sure why I didn't think of that
    before. Mann that means I have to come up with path calculations... Or maybe
    greedy is "good enough".

---
+++ js/actors/player.js
@@ -0,0 +1,89 @@
+"use strict";
+
+// @returns [-1,4] -- [NoMove, N, E, S, W]
+function randomDirection() {
+  return Math.floor(Math.random() * 5) - 1;
+}
+
+function drawCircle(cxt, x, y, radius, color) {
+  cxt.fillStyle = color;
+  cxt.beginPath();
+  // a circle is an arc from 0 to 2Pi
+  cxt.arc(x * radius * 2 + radius,
+          y * radius * 2 + radius,
+          radius, 0, Math.PI * 2, true);
+
+  cxt.closePath();
+  cxt.fill();
+}
+
+function Player(id, x, y, radius, color) {
+  this.id = id;
+
+  this.radius = radius || 16;
+
+  this.x = x;
+  this.y = y;
+
+  this.oldX = x;
+  this.oldY = y;
+
+  if (this.x < 0) {
+    this.x = 0;
+  }
+
+  if (this.x > screen.cols) {
+    this.x = screen.cols;
+  }
+
+  if (this.y < 0) {
+    this.y = 0;
+  }
+  if (this.y > screen.rows) {
+    this.y = screen.rows;
+  }
+
+  this.color = color || "#000000";
+
+  this.draw = function (cxt) {
+    drawCircle(cxt, this.x, this.y, this.radius, this.color);
+  };
+
+  this.collide = function (actor) {
+    return defaultCollision(this, actor);
+  };
+
+  this.rewind = function () {
+    this.x = this.oldX;
+    this.y = this.oldY;
+  };
+
+  // the player doesn't take automatic actions
+  this.act = function () {
+  };
+
+  this.left = function () {
+    this.oldX = this.x;
+    this.oldY = this.y;
+    this.x = this.x - 1;
+  };
+
+  this.right = function () {
+    this.oldX = this.x;
+    this.oldY = this.y;
+    this.x = this.x + 1;
+  };
+
+  this.up = function () {
+    this.oldX = this.x;
+    this.oldY = this.y;
+    this.y = this.y - 1;
+  };
+
+  this.down = function () {
+    this.oldX = this.x;
+    this.oldY = this.y;
+    this.y = this.y + 1;
+  };
+}
+

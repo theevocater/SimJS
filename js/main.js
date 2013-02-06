476c008602f39eaa5b91d527f2e50081cec7dad1
Jake Kaufman <theevocater@gmail.com>
2013-02-06 01:07:55 -0800

    added some require todos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 require(["jquery-1.8.3.min", "underscore-min"], function ($, _) {
 
+  // need to make modules/clean up dependencies
+  // TODO probably move these into an "actors" file and declare each one
+  // separately? I think that is correct?
   require(["actors/actor"]);
   require(["actors/wall","actors/enemy","actors/player"]);
 
+  // TODO relatedly, modulize these
   require(["log"]);
   require(["board"]);
   require(["run"]);

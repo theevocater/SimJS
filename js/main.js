d14b235244e7abd19989847fc89fa693006a0b27
Jake Kaufman <theevocater@gmail.com>
2013-02-06 11:11:18 -0800

    Modules should be 1 per file
    Apparently the file system is how modules are defined so I was wrong. They
    should be one per file.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 require(["jquery-1.8.3.min", "underscore-min"], function ($, _) {
 
   // need to make modules/clean up dependencies
-  // TODO probably move these into an "actors" file and declare each one
-  // separately? I think that is correct?
   require(["actors/actor"]);
   require(["actors/wall","actors/enemy","actors/player"]);
 

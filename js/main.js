fadfd84ae8d3b59c67e652a660ee38bf4c8586d7
Jake Kaufman <theevocater@gmail.com>
2013-02-08 17:55:33 -0800

    Clean up main.js a bit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-require(["jquery-1.8.3.min", "underscore-min", "sprite_cache"], function ($, _, sprites) {
+require(["jquery-1.8.3.min", "underscore-min"], function ($, _) {
+  require(["sprite_cache"], function (sprites) {
 
-require(['sprite_cache']);
-  // need to make modules/clean up dependencies
-  require(["actors/actor"]);
-  require(["actors/wall","actors/enemy","actors/player"]);
+    // need to make modules/clean up dependencies
+    require(["actors/actor"]);
+    require(["actors/wall","actors/enemy","actors/player"]);
 
-  // TODO relatedly, modulize these
-  require(["log"]);
-  require(["board"]);
-  require(["run"]);
+    // TODO relatedly, modulize these
+    require(["log"]);
+    require(["board"]);
+    require(["run"]);
+  });
 });

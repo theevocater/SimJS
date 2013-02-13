19b46d755041c2f7308f4affd3ce30ccf9aee8fc
Jake Kaufman <theevocater@gmail.com>
2013-02-13 02:51:46 -0800

    Add use strict/comment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+"use strict";
+
+// this is not ideal, should be fixed up
 require(["jquery-1.8.3.min", "underscore-min"], function ($, _) {
   require(["sprite_cache"], function (sprites) {
     require(["run"]);
